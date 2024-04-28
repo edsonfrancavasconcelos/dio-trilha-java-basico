5120bef1b4d784088402d00f7375d0cd8ef9c560
EdsonVasconcelos <vascofrancca@icloud.com>
2024-04-28 17:31:15 -0300

    Alteracoes para aprender

--- tipos-variaveis/src/TipoEVariaveis.java
+++ tipos-variaveis/src/TipoEVariaveis.java
@@ -1,14 +1,9 @@
 public class TipoEVariaveis {
     public static void main(String[] args) throws Exception {
-       short numeroCurto = 1;
-       int numeroNormal = numeroCurto;
-       short numeroCurto2 = (short) numeroNormal;
+       
        String meuNome = "Edson Vasconcelos";
        System.out.println(meuNome);
-  
 
-        final double VALOR_DE_PI = 3.14;
-              
-        System.out.println(VALOR_DE_PI);
+      
     }
 }
