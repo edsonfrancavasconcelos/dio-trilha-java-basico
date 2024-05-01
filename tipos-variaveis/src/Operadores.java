8af775c98e1128de37ea7e83264a5996948e36c1
EdsonVasconcelos <vascofrancca@icloud.com>
2024-05-01 19:08:58 -0300

    Atualizações do estudo

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,79 @@
+public class Operadores {
+
+  public static void main(String[] args) {
+    String nomeCompleto = "LINGUAGEM " + "JAVA";//CONCATENADO
+
+    boolean condicaoUm = true;
+    boolean condicaoDois = true;
+
+    if(condicaoUm && 7>5){
+      System.out.println("As duas condições são verdadeiras");
+   
+    } 
+
+    if(condicaoUm || condicaoDois){
+      System.out.println("Uma das duas condições é verdadeira");
+   
+    } 
+    System.out.println("Fim");
+    //System.out.println(nomeCompleto);
+
+    //String nomeUm = "Edson";
+    //String nomeDois = "Edson";
+    //String nomeDois = new String("Edson");
+
+ //System.out.println(nomeUm == nomeDois);
+  //System.out.println(nomeUm.equals(nomeDois));
+
+    //int numero1 =1;
+    //nt numero2 =2;
+
+    //boolean simNao = numero1 == numero2;
+
+    //int a,b;
+    //a=8;
+    //b=8;
+
+    //String resultado = a==b ? "VERDADEIRO" : "FALSO"; //OTIMIZEI O CÓDIGO,PORÉM O DE BAIXO TAMBÉM ESTÁ CORRETO.
+    //int resultado = a==b ? 1 : 0;
+    //if(a==b)
+    //resultado = "VERDADEIRO";
+   //else
+    //resultado = "FALSO";
+
+    //if(numero1 < numero2){
+      //System.out.println("A nossa condição é verdadeira");
+    }
+
+    //System.out.println("NúmeroUm é maior a númeroDois? " + simNao);
+
+    //simNao = numero1 != numero2;
+    //simNao = numero1 > numero2;
+
+    //System.out.println("numeroUm é diferente de numeroDois? "+ simNao);
+
+
+    //boolean variavel = false;
+    //System.out.println(!variavel);
+    //variavel = !variavel;
+    //System.out.println(variavel);
+
+    //int numero = 5;
+
+    // numero = -numero;
+    // System.out.println(numero);
+
+    // numero = numero * -1;
+
+    // System.out.println(numero);
+
+    // numero = numero +1
+    // numero++;
+    // System.out.println(++ numero);
+    // System.out.println(--numero);
+
+    // System.out.println(numero);
+
+  }
+
+
