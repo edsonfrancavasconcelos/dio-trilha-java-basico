fff284bb332d0527668381993a3686331c1846fe
EdsonVasconcelos <vascofrancca@icloud.com>
2024-05-02 07:50:13 -0300

    Aula de metodos concluida

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,22 @@
+public class Usuario {
+
+  public static void main(String[] args) {
+
+    SmartTv smartTv = new SmartTv();
+
+    smartTv.mudarCanal(12);
+    System.out.println("Canal: " + smartTv.canal);
+
+    smartTv.diminuirVolume();
+    smartTv.diminuirVolume();
+    smartTv.aumentarVolume();
+    System.out.println("Volume atual " + smartTv.volume);
+
+    smartTv.ligar();
+    System.out.println("Novo Status -> Ligada? " + smartTv.ligada);
+
+    smartTv.desligar();
+    System.out.println("Novo Status -> Ligada? " + smartTv.ligada);
+  }
+
+}
