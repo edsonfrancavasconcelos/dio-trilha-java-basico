ee323a3574c1dccd0d0c646d97b3b372bc053979
EdsonVasconcelos <vascofrancca@icloud.com>
2024-05-05 19:43:10 -0300

    Fim desta  jornada

---
+++ controlo-fluxo/src/planoOperadora.java
@@ -0,0 +1,16 @@
+public class planoOperadora {
+
+  public static void main(String[] args) {
+
+    String plano = "T";
+
+    if (plano == "B") {
+      System.out.println("Aqui você terá 100 minutos de ligação");
+    } else if (plano == "M") {
+      System.out.println("Aqui você terá 100 minutos de ligação + WhatsApp e Instagram grátis");
+    } else if (plano == "T") {
+      System.out.println("Aqui você terá 100 minutos de ligação + WhatsApp e Instagram e ainda 5Gb no YouTube grátis");
+
+    }
+  }
+}
---
+++ controlo-fluxo/src/planoOperadora2.java
@@ -0,0 +1,20 @@
+public class planoOperadora2 {
+
+  public static void main(String[] args) {
+
+    String plano = "M";
+
+    switch (plano) {
+      case "T": {
+        System.out.println("5Gb de YouTube");
+      }
+      case "M": {
+        System.out.println("WhatsApp e Instagram grátis");
+      }
+      case "B": {
+        System.out.println("100 Minutos de ligações grátis");
+      }
+
+    }
+  }
+}
